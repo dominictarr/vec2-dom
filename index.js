7ca80dc573e355994a6d9e54b22390746e1ea1fc
Dominic Tarr <dominic.tarr@gmail.com>
2013-11-09 09:40:15 +0800

    if you are going to bind, set position immediately, so that it has the right size

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ var mouse, scroll, screen, size
 var element =
 exports.element = function (el, bind) {
   var rec = el.getBoundingClientRect()    
-  var rec2 = new Rec2
+  var rec2 = new Rec2()
   
   var style = getComputedStyle(el)
-  console.log(style)
+
   rec2.set((rec.left || 0)  - (parseFloat(style['margin-left']) || 0)
     , (rec.top || 0) - (parseFloat(style['margin-top']) || 0)) 
   //check if it's actually a Rec2 - if it's a vec2
   //skip this step.
+
   if(rec2.size)
     rec2.size.set(rec.width, rec.height)
 
@@ -84,14 +85,17 @@ exports.screenSize = function () {
 
 //if bind=true this will make the element
 //track the position of the Vec2,
-//and will work around the DOM qwerk that
 
 exports.absolute = function (el, bind) {
+  //if we are binding, it changes the layout rules
+  //for say, divs, so do that first.
+  if(bind)
+    el.style.position = 'absolute'
+
   var absolute =
     element(el).subtract(element(el.parentElement))
 
   if(bind) {
-    el.style.position = 'absolute'
     function place () {
       el.style.left = absolute.x + 'px'
       el.style.top  = absolute.y + 'px'
